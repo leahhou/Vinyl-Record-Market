d03419cfbb248bc77b222de67117c21de9d7ff58
Bruce Mc Clure <brucemcc90@gmail.com>
2019-04-30 16:01:21 +1000

    albums seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 1..20
+    Listing.create(
+        title: Faker::Music.album
+        artist: Faker::Music.band
+        
+        year: (1900..2019).to_a.sample
+        format: (1..3).to_a.sample
+        price: (5..2000).to_a.sample
+        condition: (1..5).to_a.sample
+        description: Faker::TvShows::GameOfThrones.quote
+    )
+    puts "Created #{i} albums"
+end
+
+for i in 1..20
+    User.create(
+        username: Faker::Name.unique.name
+        email: Faker::Internet.unique.email
+        password: "cheese"
+    )
