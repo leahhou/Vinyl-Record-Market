904aebde9ad62ca9cd1371e209b472bff09ce7dc
Alex Baulderstone <alexbaulderstone@gmail.com>
2019-04-30 14:45:21 +1000

    setup devise and user model with username

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resource :listings
+get "/", to: "pages#home", as: "root"
+  resource :listings
 end
