a9fca786c11e0ebda5ec2eddb5ed2560349d0042
leahhou <hsx_124@hotmail.com>
2019-05-01 14:28:52 +1000

    continue on listing_controller fixing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,12 +1,24 @@
 class ListingsController < ApplicationController
-before_action :set_genre_format_and_condition, only: [:new, :edit]
+    before_action :set_listing, only: [:show, :edit, :update, :destroy]    
+    before_action :set_genre_format_and_condition, only: [:new, :edit]
+
 
     def index 
         #shows all listings
+        @listings = Listing.all
     end
 
     def create 
         #create new listing
+        p current_user
+        @listing = Listing.create(listing_params)
+        if @listing.errors.any?
+            set_genre_format_and_condition
+            render "new"
+        else
+            redirect_to listing_path
+        end
+        
     end  
 
     def new 
@@ -16,10 +28,22 @@ before_action :set_genre_format_and_condition, only: [:new, :edit]
 
     def show 
         #view a single listing
+
     end  
 
     def update 
         #updates the current listing
+        @listing = Listing.find(params[:id])
+        @listing.update(artist: params[:artist],
+        title: params[:title],
+        format_id: params[:format_id],
+        year: params[:year],
+        price: params[:price],
+        condition: params[:condition],
+        description: params[:description]
+
+        )
+        redirect_to listings_path
     end
 
     def edit 
@@ -28,6 +52,8 @@ before_action :set_genre_format_and_condition, only: [:new, :edit]
 
     def destroy 
         #deletes current listings
+        @listing.destroy
+        redirect_to listings_path
     end  
         
     private
@@ -40,5 +66,9 @@ before_action :set_genre_format_and_condition, only: [:new, :edit]
         @genres = Genre.all
         @formats = Format.all
         @conditions = Listing.conditions.keys
+    end
+
+    def listing_params
+        params.require(:listing).permit(:artist, :title, :year, :format_id, :price, :condition, :description)
     end
 end
