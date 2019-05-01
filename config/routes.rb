8568cacb43ed48c6f328d362b410977f7aa37b3a
Alex Baulderstone <alexbaulderstone@gmail.com>
2019-05-01 12:09:12 +1000

    added avatar upload functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get "/", to: "pages#home", as: "root"
   resource :listings
+  get "/users", to: "users#index", as: "users"
+  get "/users/:id", to: "users#show"
 end
