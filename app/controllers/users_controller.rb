38f5bbda40cd644118fe73602f1216a5311f801a
Alex Baulderstone <alexbaulderstone@gmail.com>
2019-05-03 11:10:25 +1000

    updated logic in user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,31 @@
 class UsersController < ApplicationController
-
+before_action :set_user, only: [:show]
+before_action :set_user_listings, only: [:show]
 def index 
 @users = User.all
 end 
+
 def show 
-# we probably don't need create @user as we can acces the info
-#through current_user
-id = params[:id]
-@user = User.find(id)
 end
 
 def userprofile
     if current_user
         id = current_user.id
         @user = User.find(id)
+        set_user_listings
     else 
         redirect_to new_user_session_path
     end
+end
+
+private
+def set_user
+    id = params[:id]
+    @user = User.find(id)
+end
+
+def set_user_listings
+    @user_listings = @user.listings
 end
 
 end
