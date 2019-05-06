c1574094d632891fa02e20157152234f1e9bec36
Alex Baulderstone <alexbaulderstone@gmail.com>
2019-05-06 10:57:15 +1000

    built basic messaging system

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_041904) do
+ActiveRecord::Schema.define(version: 2019_05_06_002407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2019_05_02_041904) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "receiver_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "formats", force: :cascade do |t|
@@ -72,6 +79,17 @@ ActiveRecord::Schema.define(version: 2019_05_02_041904) do
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "conversation_id"
+    t.bigint "user_id"
+    t.boolean "read", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "purchases", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "listing_id"
@@ -102,6 +120,8 @@ ActiveRecord::Schema.define(version: 2019_05_02_041904) do
   add_foreign_key "genres_listings", "listings"
   add_foreign_key "listings", "formats"
   add_foreign_key "listings", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
   add_foreign_key "purchases", "listings"
   add_foreign_key "purchases", "users"
 end
