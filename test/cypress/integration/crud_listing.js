65f5136664833a169ab9064a3db63974a414d11a
leahhou <hsx_124@hotmail.com>
2019-05-07 22:48:38 +1000

    create and update listing auto testing done

--- test/cypress/integration/crud_listing.js
+++ test/cypress/integration/crud_listing.js
@@ -1,4 +1,5 @@
 describe('Rails Listing CRUD', function() {
+
     beforeEach(function() {
         cy.login()
     })
@@ -15,6 +16,8 @@ describe('Rails Listing CRUD', function() {
 
            cy.get("[name='listing[year]']").type("1993"); // type in "1993" into year
 
+           cy.get("[name='listing[price]']").type("200"); // type in "1993" into year
+
            cy.get("[type='radio']").check("good"); // choose "wore" in condition
 
            cy.get("[name='listing[description]']").type("Pick Up Only"); // type in "Pick Up Only" into description.
@@ -27,23 +30,31 @@ describe('Rails Listing CRUD', function() {
 
     });
 
-    // it("update a listing", function() {
+    it("update a listing", function() {
         
-    //     cy.contains("create a new listing").click(); //click "create a new listing" button to go to new listing page
-    
-    //     cy.get("[name='listing[artist]']").type("Radiohead"); // type in "Al Green" into artist
+         cy.visit("/"); 
+
+         cy.contains("go to album").click()
+
+         cy.contains("edit album").click(); 
+        cy.get("[name='listing[artist]']").type("The Beatles"); 
      
-    //     cy.get("[name='listing[title]']").type("Pablo Honey "); // type in "Pablo Honest" into artist
+        cy.get("[name='listing[title]']").type("Hey Jude"); 
+        cy.get('select').select('2LP').should('have.value', '1'); 
 
-    //     cy.get('select').select('2LP').should('have.value', '1'); // select "2LP" from format dropdown menu
+        cy.get("[name='listing[year]']").type("1993"); 
 
-    //     cy.get("[name='listing[year]']").type("1993"); // type in "1993" into year
+        cy.get("[type='radio']").check("good"); 
 
-    //     cy.get("[type='radio']").check("good"); // choose "wore" in condition
+        cy.get("[name='listing[description]']").type("Pick Up Only"); 
 
-    //     cy.get("[name='listing[description]']").type("Pick Up Only"); // type in "Pick Up Only" into description.
-    // });
+        cy.contains("Update Listing").click(); // click "create listing" button to submit 1st part of the form
 
+        cy.get("[type='checkbox']").check(['4', '2']); // choose "wore" in condition
+
+        cy.contains("Update Listing").click(); // click "update listing" button to submit 2nd part of the 
+
+    });
     // it("delete a listing", function() {
         
     //     cy.visit("/users/sign_in"); // go to sign up page correctly
