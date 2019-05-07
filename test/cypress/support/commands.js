c1c9a03322ad362946ffa2e50420516c2d0c4799
leahhou <hsx_124@hotmail.com>
2019-05-07 16:32:59 +1000

    log in function added for testing

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -23,3 +23,15 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', () => {
+    cy.visit("/users/sign_in"); // go to sign up page correctly
+            
+            
+        cy.get("[name='user[login]']").type("leah"); // select username field and type in leah
+        
+        cy.get("[name='user[password]']").type("123456"); // select password field and type in password
+
+        cy.get("[name='commit']").click(); //click "sign up" button to sumbit registration form
+        
+        cy.contains("Signed in successfully."); //automatically redirect to home and show successfully log in
+});
