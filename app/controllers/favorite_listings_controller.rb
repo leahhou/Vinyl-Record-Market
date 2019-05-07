9df64d272e55d354b42f4d1292580f60fc3d33b6
Alex Baulderstone <alexbaulderstone@gmail.com>
2019-05-07 15:12:59 +1000

    removed byebug

--- app/controllers/favorite_listings_controller.rb
+++ app/controllers/favorite_listings_controller.rb
@@ -2,7 +2,6 @@ class FavoriteListingsController < ApplicationController
     before_action :set_listing
     
     def create
-      byebug
       if Favorite.create(favorited: @listing, user: current_user)
         @listing.favorite_count += 1 
         @listing.save 
@@ -13,7 +12,6 @@ class FavoriteListingsController < ApplicationController
     end
     
     def destroy
-      byebug
       Favorite.where(favorited_id: @listing.id, user_id: current_user.id).first.destroy
       @listing.favorite_count -=1
       @listing.save
