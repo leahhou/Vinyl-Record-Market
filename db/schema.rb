0b179dce5a299122ce6a947c01d574e68fbcd3ca
leahhou <hsx_124@hotmail.com>
2019-05-07 14:57:25 +1000

    add before actions and fix seed files in testing environment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_06_002407) do
+ActiveRecord::Schema.define(version: 2019_05_07_020344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 2019_05_06_002407) do
     t.integer "receiver_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "favorited_type"
+    t.bigint "favorited_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["favorited_type", "favorited_id"], name: "index_favorites_on_favorited_type_and_favorited_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "formats", force: :cascade do |t|
@@ -75,6 +85,7 @@ ActiveRecord::Schema.define(version: 2019_05_06_002407) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "favorite_count", default: 0
     t.index ["format_id"], name: "index_listings_on_format_id"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
@@ -116,6 +127,7 @@ ActiveRecord::Schema.define(version: 2019_05_06_002407) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "favorites", "users"
   add_foreign_key "genres_listings", "genres"
   add_foreign_key "genres_listings", "listings"
   add_foreign_key "listings", "formats"
