1a7ed53df7c024023d6ad47f2a742898c21de038
Bruce Mc Clure <brucemcc90@gmail.com>
2019-05-08 14:50:21 +1000

    commit before merge with dev

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,10 @@ class PagesController < ApplicationController
       def home 
         @most_liked_listings = Listing.all.sort_by {|listing| listing.favorite_count}.last(3).reverse
         @home_listings = Listing.last(3).reverse
+
+
+        @q = User.ransack(params[:q])
+        @results = @q.result(distinct: true).includes(:listing)
       end
 
 end
