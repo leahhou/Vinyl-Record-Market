dacafa9a8bb71da7fe5105cf0e9630840ce885ce
Alex Baulderstone <alexbaulderstone@gmail.com>
2019-05-08 12:44:40 +1000

    added top 3 most popular records

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
       def home 
-        @home_listings = Listing.last(3)
+        @most_liked_listings = Listing.all.sort_by {|listing| listing.favorite_count}.last(3).reverse
+        @home_listings = Listing.last(3).reverse
       end
+
 end
