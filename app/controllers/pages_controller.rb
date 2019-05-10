b8d3620f6c7012537e60ad9c7bdff60843f4df3e
Alex Baulderstone <alexbaulderstone@gmail.com>
2019-05-10 13:03:42 +1000

    fixed spelling error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home 
-    if Listings.all != 0 
+    if Listing.all != 0 
     @most_liked_listings = Listing.all.sort_by {|listing| listing.favorite_count}.last(3).reverse
     @home_listings = Listing.last(3).reverse
   
