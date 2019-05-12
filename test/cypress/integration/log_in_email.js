036e334ee7732880d7246bbac4495a739f20cf89
Alex Baulderstone <alexbaulderstone@gmail.com>
2019-05-12 12:19:51 +1000

    fixed some more typos

--- test/cypress/integration/log_in_email.js
+++ test/cypress/integration/log_in_email.js
@@ -1,15 +1,10 @@
 describe('Rails Log In', function() {
     it("Log in with username", function() {
         
-        cy.visit("/users/sign_in"); // go to sign up page correctly
-        
-        
-        cy.get("[name='user[login]']").type("hsx_124+13@hotmail.com"); // select username field and type in leah
-        
-        cy.get("[name='user[password]']").type("123456"); // select password field and type in password
-
-        cy.get("[name='commit']").click(); //click "sign up" button to sumbit registration form
-        
-        cy.contains("Signed in successfully."); //automatically redirect to home and show successfully log in
+        cy.visit("/users/sign_in"); 
+        cy.get("[name='user[email]']").type("alex@test.com"); 
+        cy.get("[name='user[password]']").type("alex1234"); 
+        cy.get("[name='commit']").click(); 
+        cy.contains("Signed in successfully."); 
     });
 });
